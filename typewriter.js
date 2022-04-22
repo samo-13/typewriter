e8f6a762ab7a67764d7818bdfd696af29c1b5b25
Sarah Moss <sarah-moss@live.com>
2022-04-22 02:07:31 +0000

    Eslint code

--- typewriter.js
+++ typewriter.js
@@ -1,18 +1,19 @@
-let sentence = "hello there from lighthouse labs";
+// let sentence = "hello there from lighthouse labs";
 let delay = 0;
-
 
 let typewriter = function(sentence) {
   for (const char of sentence) {
     setTimeout(() => {
       // process.stdout.write instead of console.log to have more control and avoid automatically adding an extra "newline character" at the end each time.
       process.stdout.write(char);
-    }, delay += 50) // increments the delay
+    }, delay += 50); // increments the delay
   }
+  // console.log('\n')
   setTimeout(() => {
     // process.stdout.write instead of console.log to have more control and avoid automatically adding an extra "newline character" at the end each time.
     process.stdout.write('\n');
-  }, sentence.length * 100) // adds new line when the above is complete
-};
+  }, sentence.length * 100); // adds new line when the above is complete
+}
 
-typewriter(`Hi, I'm Sarah`)
+typewriter(`Hi, I'm Sarah`);
+// typewriter("hello there from lighthouse labs");
